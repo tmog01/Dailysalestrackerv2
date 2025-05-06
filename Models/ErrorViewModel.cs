29fc418d555b37396dd3c693908b70cdbe46dc91
mohanrav <mohanrav@gmail.com>
2025-05-06 13:04:20 -0400

    Updates
    Updating

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DailySalesTracker.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
